a6cbe688caea10f7fac00cf3b0a8f282a95c46df
rosindubh <p.welsby2@ntlworld.com>
2021-08-29 12:35:52 +0100

    added deletLast function and set onClick={deleteLast} on DEL button

--- src/App.js
+++ src/App.js
@@ -42,6 +42,15 @@ function App() {
     setCalc(eval(calc).toString());
   }
 
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  }
+
   return (
     <div className="App">
       <div className="calculator">
@@ -57,7 +66,7 @@ function App() {
           <button onClick={() => updateCalc('-')}>-</button>
 
 
-          <button>DEL</button>
+          <button onClick={deleteLast}>DEL</button>
         </div>
 
         <div className="digits">
